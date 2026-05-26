196035399eb5e3ecb87a4c36782dd488f3ba4a2d
humano-platform <64204848+BanderaS1988@users.noreply.github.com>
2026-05-26 07:51:10 +0200

    Update robots.js

--- api/robots.js
+++ api/robots.js
@@ -5,30 +5,46 @@ module.exports = (req, res) => {
 User-agent: *
 Allow: /
 Disallow: /admin/
+Disallow: /_next/static/chunks/
+Disallow: /api/sitemap
+Disallow: /api/news-sitemap
 Crawl-delay: 1
+
 User-agent: Googlebot
 Allow: /
+Disallow: /_next/static/chunks/
 Crawl-delay: 0
+
 User-agent: Googlebot-News
 Allow: /
+
 User-agent: GPTBot
 Allow: /
+
 User-agent: ChatGPT-User
 Allow: /
+
 User-agent: PerplexityBot
 Allow: /
+
 User-agent: ClaudeBot
 Allow: /
+
 User-agent: Google-Extended
 Allow: /
+
 User-agent: Amazonbot
 Allow: /
+
 User-agent: Applebot-Extended
 Allow: /
+
 User-agent: Diffbot
 Allow: /
+
 User-agent: cohere-ai
 Allow: /
+
 Sitemap: ${SITE}/sitemap.xml
 Sitemap: ${SITE}/news-sitemap.xml
 `;
